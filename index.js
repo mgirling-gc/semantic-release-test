0989bce95824ef039cb0282c9bb8a9b1559a4a00
Miranda Girling <mgirling@gocardless.com>
2025-05-23 13:07:38 +0100

    feat: add user prompt

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const readLine = require('readline');
+
 console.log("Hello Universe!");
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Hello, GoCardless User!');
+
+rl.question('What is your name? ', (name) => {
+  console.log(`Hello, ${name}!`);
+  rl.close();
+});
